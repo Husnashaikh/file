572c33ee8d786bcf3dad93334d6da1e11449e808
Shaikh Husna <95674265+Husnashaikh@users.noreply.github.com>
2022-06-23 10:21:40 +0530

    Add files via upload

---
+++ meraki_bank_list.py
@@ -0,0 +1,3 @@
+bank_list=open("bank_list.text","w")
+bank_list.write("Kotak\nHDFC\nRBL\nSBI\nBank of Baroda")
+bank_list.close()
---
+++ practice_question.py
@@ -0,0 +1,12 @@
+my_file=open("my_name.text","w")
+my_file.write("husna\npinki\nmosu\nsarita")
+my_file.close()
+
+
+
+
+
+
+
+
+
